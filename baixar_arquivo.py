ca989e59ec39b12aec02088a275d92571145ddf6
Vinicios <vinicios.ednei.r.a.a@gmail.com>
2022-07-02 19:51:37 -0300

    primeira versao de download do covid

---
+++ baixar_arquivo.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import chromedriver_autoinstaller
+import test
+
+chromedriver_autoinstaller.install()
+
+chrome_options = webdriver.ChromeOptions()
+prefs = {'download.default_directory' : 'C:\\Users\\user\\dev\\covid'}
+chrome_options.add_experimental_option('prefs', prefs)
+
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+driver.get("https://covid.saude.gov.br/")
+driver.find_element(By.XPATH,'/html/body/app-root/ion-app/ion-router-outlet/app-home/ion-content/div[1]/div[2]/ion-button').click()
+input('Espere')
+test.printar_buceta()
---
+++ test.py
@@ -0,0 +1,6 @@
+from asyncio.sslproto import _DO_HANDSHAKE
+from msilib import datasizemask
+
+
+def printar_buceta():
+    print('buceta')
